06bff1909d87cc0bb505b7286edc6617288f1027
Marcos Medeiros <93498592+msrcos3s@users.noreply.github.com>
2021-11-19 15:45:23 -0300

    Assignment Submission 2
    Lexical Scoping. Week 3. Peer-graded Assignment. Data Science.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## It results in a inverse matrix object. 
+## Example: pmatrix <- makeCacheMatrix(matrix(1:4, nrow=2, ncol=2))
+## cacheSolve(pmatrix)
+## pmatrix$getInverse()
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y){
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() {x}
+        setInverse <- function(inverse) {inv <<- inverse}
+        getInverse <- function() {inv}
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+        inv <- x$getInverse()
+        if(!is.null(inv)){
+                message("obtendo a matriz inversa")
+                return(inv)
+        }
+     mat <- x$get()
+     inv <- solve (mat, ...)
+     x$setInverse(inv)
+     inv
 }
